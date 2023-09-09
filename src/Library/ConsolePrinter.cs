5a80db4443a59b5b34b066364606b38577597ade
Guillermo Acquistapace <guillermoacq@gmail.com>
2023-09-09 15:28:53 -0300

    Exercise completed

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class ConsolePrinter
+    {
+        public static void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+
+            foreach (Step step in recipe.GetSteps())
+            {
+            Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+
+        }
+    }
+}
